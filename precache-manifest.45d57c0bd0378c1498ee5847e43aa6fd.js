c2059193f3310332ebf2d52f9c5d2d004168f908
Ting Zhou <limtingzhou@gmail.com>
2019-01-29 00:06:20 +0800

    Updates

--- precache-manifest.45d57c0bd0378c1498ee5847e43aa6fd.js
+++ precache-manifest.45d57c0bd0378c1498ee5847e43aa6fd.js
@@ -4,15 +4,19 @@ self.__precacheManifest = [
     "url": "/youtube-search-react/static/js/runtime~main.3c35be84.js"
   },
   {
-    "revision": "666d123149be4ac15556",
-    "url": "/youtube-search-react/static/js/main.666d1231.chunk.js"
+    "revision": "e7cf7d004a3a4ea70169",
+    "url": "/youtube-search-react/static/js/main.e7cf7d00.chunk.js"
   },
   {
     "revision": "102c3bf24ee7bc42981e",
     "url": "/youtube-search-react/static/js/1.102c3bf2.chunk.js"
   },
   {
-    "revision": "d06c8e76a2dc6a38ac961059e11c62f2",
+    "revision": "e7cf7d004a3a4ea70169",
+    "url": "/youtube-search-react/static/css/main.e76a0262.chunk.css"
+  },
+  {
+    "revision": "67d8e80a692da17c6f647837c13b251e",
     "url": "/youtube-search-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/youtube-search-react/precache-manifest.68a4cbeb9983d9108c86bc6ae9627b58.js"
+  "/youtube-search-react/precache-manifest.45d57c0bd0378c1498ee5847e43aa6fd.js"
 );
 
 workbox.clientsClaim();
